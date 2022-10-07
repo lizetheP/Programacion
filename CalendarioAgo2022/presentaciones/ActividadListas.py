6efa080ea894350196330e1ad984e49fb16322f1
Lizethe <lizetheperez@gmail.com>
2022-10-06 22:34:24 -0500

    subir

--- CalendarioAgo2022/presentaciones/ActividadListas.py
+++ CalendarioAgo2022/presentaciones/ActividadListas.py
@@ -5,10 +5,9 @@ import statistics
 
 calificaciones = [100, 90, 87, 88, 100, 70, 80, 100]
 print("Mis calificaciones son: ", calificaciones)
-print("Mi promedio es : %.1f" % statistics.mean(calificaciones))
-print("La moda de mis calificaciones es : %.1f" % statistics.mode(calificaciones))
 print("La máxima calificación que obtuve fue : %.1f" % np.max(calificaciones))
 print("La mínima calificación que obtuve fue : %.1f " % np.min(calificaciones))
+print("Mi promedio es : %.1f" % statistics.mean(calificaciones))
 print("La desviación de mis notas fue : %.1f" % statistics.stdev(calificaciones))
 print("Obtuve %i calificaciones de 100" % calificaciones.count(100))
 print("Obtuve %i calificaciones de 80" % calificaciones.count(80))
