826e630212c9ba511d55d3ca1e9120ee31e67244
Lizethe <lizetheperez@gmail.com>
2023-10-03 10:34:09 -0600

    subir

--- CalendarioAgo2023/actividades/Actividad7.py
+++ CalendarioAgo2023/actividades/Actividad7.py
@@ -3,12 +3,12 @@ import openpyxl
 
 tabla = pd.read_excel("vendedores.xlsx")
 print(tabla)
-
+"""
 nombres = tabla["NOMBRE"]
 salarios = tabla["SALARIO"]
 print(nombres)
 
 salarioMax = salarios.max()
 print("El salario más grande es: $%.2f" % salarioMax)
-
+"""
 
--- CalendarioAgo2023/actividades/cicloWhile_Ago23completo.py
+++ CalendarioAgo2023/actividades/cicloWhile_Ago23completo.py
@@ -1,3 +1,10 @@
+def tabla_multiplicar(n):
+    cont = 1
+    while cont <= 10:
+        res = cont * n
+        print("%i x %i = %i" % (cont, n, res))
+        cont = cont + 1
+
 def cuenta_suma():
     suma = 0
     cont = 0
@@ -8,13 +15,34 @@ def cuenta_suma():
     print("La suma es: ", suma)
     print("La cantidad de números fue: ", cont)
             
+def comprueba_codigo (clave):
+    password = input("Dame tu password: ")
+    while clave != password:
+        print("Clave incorrecta, intenta de nuevo")
+        password = input("Dame tu clave: ")
+    print("Clave exitosa")
+
+def valida_codigo (clave):
+    password = input("Dame tu password: ")
+    while clave != password:
+        print("Clave incorrecta, intenta de nuevo")
+        password = input("Dame tu clave: ")
+    print("Clave exitosa")
+
 def valida_matricula (lista):
     matricula = input("Dame tu matrícula: ")
     while matricula not in lista:
         print("Matrícula inválida, intenta de nuevo")
         matricula = input("Dame tu matrícula: ")
     print("Matrícula válida")
-     
+    
+def valida_matricula0 (matricula_correcta):
+    matricula = input("Dame tu matrícula: ")
+    while matricula != matricula_correcta:
+        print("Matrícula incorrecta, intenta de nuevo")
+        matricula = input("Dame tu matrícula: ")
+    print("Matrícula correcta")
+    
 def total_pagar(n):
     cont = 1
     acum = 0
@@ -42,13 +70,35 @@ def total_depositos(n, mes_curso):
     print("Total de importes:", acum)
     print("Total de importes del mes en curso:", contdep)
 
+def clientes(n):
+    cmay = 0
+    clocal = 0
+    cforaneo = 0
+    cont = 1
+    while cont <= n:
+        edad = int(input("Edad: "))
+        ciudad = input("Ciudad: ")
+        cont = cont + 1
+        if edad >= 18: 
+            cmay = cmay + 1
+        if ciudad.lower() == "querétaro": 
+            clocal = clocal + 1
+        else:
+            cforaneo = cforaneo + 1
+    print("Total de mayores de edad:", cmay)
+    print("Total de clientes foráneos:", cforaneo)
+    print("Total de clientes locales:", clocal)
+
 def menu():
     print()
     print("1. Cuenta y suma")
     print("2. Total a pagar")
     print("3. Total de depósitos")
-    print("4. Valida matrícula 2")
-    print("5. Salir")
+    print("4. Clientes")
+    print("5. Comprueba clave")
+    print("6. Valida matrícula")
+    print("7. Valida matrícula 2")
+    print("8. Salir")
     
 def main():
     continua = True
@@ -66,9 +116,21 @@ def main():
             mes_curso = input("Dame el mes en curso: ")
             total_depositos(n, mes_curso)
         elif opcion == 4:
+            n = int(input("Dame el número de clientes: "))
+            clientes(n)
+        elif opcion == 5:
+            clave = input("Dame la clave: ")
+            comprueba_codigo(clave)
+        elif opcion == 6:
+            matricula_correcta = "A01201524"
+            valida_matricula(matricula_correcta)
+            nombre = input("Dame tu nombre: ")
+            carrera = input("Dame tu carrera: ")
+            print(matricula_correcta + " " + nombre + " " + carrera)
+        elif opcion == 7:
             lista = ["A01300000", "A01201524", "A01200000"]
             valida_matricula(lista)
-        elif opcion == 5:
+        elif opcion == 8:
             print("Adiós")
             continua = False
         else:
