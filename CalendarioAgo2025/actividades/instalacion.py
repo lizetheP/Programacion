7a131b7277ba6641e64c2e227f4b21a9b124a4f1
lizetheP <lizetheperez@gmail.com>
2025-10-06 16:24:26 -0600

    subir

---
+++ CalendarioAgo2025/actividades/instalacion.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import statistics
+import openpyxl
+
+tabla = pd.read_excel("vendedores.xlsx")
+print(tabla)
